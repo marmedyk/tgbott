7195d79769154d572cacbea70f16209a98540343
Marko Medyk <86977346+marmedyk@users.noreply.github.com>
2023-09-11 19:07:37 +0200

    Add files via upload

---
+++ Shop-bot/app.py
@@ -0,0 +1,91 @@
+
+import os
+import handlers
+from aiogram import executor, types
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from data import config
+from loader import dp, db, bot
+import filters
+import logging
+
+filters.setup(dp)
+
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.environ.get("PORT", 5000))
+user_message = 'User'
+admin_message = 'Admin'
+
+
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message):
+
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    markup.row(user_message, admin_message)
+
+    await message.answer('''Hello! 👋
+
+🤖 I'm shop bot to sell any goods
+    
+🛍️ To enter menu and choose some items you want use command /menu.
+
+💰 You can top up your balance with ___.
+
+❓ Use /sos to contact admins that'll help you asap.
+
+    ''', reply_markup=markup)
+
+
+@dp.message_handler(text=user_message)
+async def user_mode(message: types.Message):
+
+    cid = message.chat.id
+    if cid in config.ADMINS:
+        config.ADMINS.remove(cid)
+
+    await message.answer('User mode turned on.', reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handler(text=admin_message)
+async def admin_mode(message: types.Message):
+
+    cid = message.chat.id
+    if cid not in config.ADMINS:
+        config.ADMINS.append(cid)
+
+    await message.answer('Admin mode turned on.', reply_markup=ReplyKeyboardRemove())
+
+
+async def on_startup(dp):
+    logging.basicConfig(level=logging.INFO)
+    db.create_tables()
+
+    await bot.delete_webhook()
+    await bot.set_webhook(config.WEBHOOK_URL)
+
+
+async def on_shutdown():
+    logging.warning("Shutting down..")
+    await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+    logging.warning("Bot down")
+
+
+if __name__ == '__main__':
+
+    if "HEROKU" in list(os.environ.keys()):
+
+        executor.start_webhook(
+            dispatcher=dp,
+            webhook_path=config.WEBHOOK_PATH,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT,
+        )
+
+    else:
+
+        executor.start_polling(dp, on_startup=on_startup, skip_updates=False)
---
+++ Shop-bot/keyboards/default/markups.py
@@ -0,0 +1,31 @@
+from aiogram.types import ReplyKeyboardMarkup
+
+back_message = '👈 Back'
+confirm_message = '✅ confirm order'
+all_right_message = '✅ Allright'
+cancel_message = '🚫 Cance;'
+
+def confirm_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(confirm_message)
+    markup.add(back_message)
+
+    return markup
+
+def back_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(back_message)
+
+    return markup
+
+def check_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(back_message, all_right_message)
+
+    return markup
+
+def submit_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(cancel_message, all_right_message)
+
+    return markup
